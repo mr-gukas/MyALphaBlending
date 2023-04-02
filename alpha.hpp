d96ba1885b542481a33f045e67e5b4d3c826dd16
mr-gukas <gukasyan.vladimir@mail.ru>
2023-04-02 14:49:06 +0300

    initial commit

---
+++ alpha.hpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <sstream> // for std::ostringstream
+#include <stdlib.h>
+
+
+
